856528794dbf821b21c01330c5ec06548b66beec
Jun-Min Lee <ljm56897@gmail.com>
2025-06-20 16:03:11 +0900

    feat: Updated file system related utilities

--- src/utils/filesys_utils.py
+++ src/utils/filesys_utils.py
@@ -1,5 +1,9 @@
 import os
 import json
+from typing import Any
+from pathlib import Path
+
+from utils import log
 
 
 
@@ -9,10 +13,28 @@ def txt_load(path: str):
     return content    
 
 
+
 def json_load(path: str):
     with open(path, 'r') as f:
         return json.load(f)
-    
+
+
+
+def yaml_save(file:str='data.yaml', data:Any=None) -> None:
+    """
+    Save data to an YAML file.
+
+    Args:
+        file (str, optional): File name. Default is 'data.yaml'.
+        data (Any, optional): Data to save in YAML format.
+    """
+    save_path = Path(file)
+    log(data.dumps())
+    with open(save_path, "w") as f:
+        f.write(data.dumps(modified_color=None, quote_str=True))
+        log(f"Config is saved at {save_path}")
+
+
 
 def get_files(path:str, ext:str=None) -> list[str]:
     """
@@ -38,3 +60,29 @@ def get_files(path:str, ext:str=None) -> list[str]:
                 files.append(os.path.join(root, filename))
     
     return files
+
+
+
+def make_project_dir(config) -> Path:
+    """
+    Make project folder.
+
+    Args:
+        config: yaml config.
+
+    Returns:
+        (path): project folder path.
+    """
+    prefix = log('Make project folder')
+    project = config.project
+    name = config.data_name
+
+    save_dir = os.path.join(project, name)
+    if os.path.exists(save_dir):
+        log(f'{prefix}: Project {save_dir} already exists. New folder will be created.')
+        save_dir = os.path.join(project, name + str(len(os.listdir(project))+1))
+    
+    os.makedirs(project, exist_ok=True)
+    os.makedirs(save_dir)
+    
+    return Path(save_dir)
