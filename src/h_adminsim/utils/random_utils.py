fb4c571dca65f1f319fd2d194c0b5f72b8229428
SeohyunKang <kang022878@gmail.com>
2026-07-20 20:56:52 +0900

    feat: Fix generating random occupation bug

--- src/h_adminsim/utils/random_utils.py
+++ src/h_adminsim/utils/random_utils.py
@@ -374,9 +374,9 @@ def generate_random_occupation(occupation_file_path: Optional[str] = None) -> st
         str: A randomly selected occupation.
     """
     if occupation_file_path == None:
-        occupation_file_path = str(resources.files("h_adminsim.assets.occupation").joinpath("occupation.json"))
+        occupation_file_path = str(resources.files("h_adminsim.assets.patient").joinpath("occupation.json"))
 
     if registry.OCCUPATION is None:
         registry.OCCUPATION = json_load(occupation_file_path)
     
-    return random.choice(registry.OCCUPATION)
+    return random.choice(list(registry.OCCUPATION))
